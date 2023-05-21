57f8bf8181c19c06a6dfabf12eabb5dc2caaed99
Vladimir Rukavishnikov <94602014+HungryVovka@users.noreply.github.com>
2023-05-21 13:36:07 +0500

    feat: 8 kyu solutions

---
+++ 8 kyu/Name Shuffler.ex
@@ -0,0 +1,37 @@
+# -----------------------------------------------------------
+# Write a function that returns a string in which firstname is swapped with last name.
+# 
+# Example(Input --> Output)
+# 
+# "john McClane" --> "McClane john"
+# -----------------------------------------------------------
+
+defmodule Messy do
+  def name_shuffler(name) do
+    name
+    |> String.split(" ")
+    |> Enum.reverse()
+    |> Enum.join(" ")
+  end
+end
+
+# -----------------------------------------------------------
+# License
+# Tasks are the property of Codewars (https://www.codewars.com/) 
+# and users of this resource.
+# 
+# All solution code in this repository 
+# is the personal property of Vladimir Rukavishnikov
+# (vladimirrukavishnikovmail@gmail.com).
+# 
+# Copyright (C) 2023 Vladimir Rukavishnikov
+# 
+# This file is part of the HungryVovka/Codewars-Elixir
+# (https://github.com/HungryVovka/Codewars-Elixir)
+# 
+# License is GNU General Public License v3.0
+# (https://github.com/HungryVovka/Codewars-Elixir/blob/main/LICENSE)
+# 
+# You should have received a copy of the GNU General Public License v3.0
+# along with this code. If not, see http://www.gnu.org/licenses/
+# -----------------------------------------------------------
