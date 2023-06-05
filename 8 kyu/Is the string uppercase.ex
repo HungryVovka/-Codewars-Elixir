bd56696941e9f220cea3c373ee90884727909bcc
Vladimir Rukavishnikov <94602014+HungryVovka@users.noreply.github.com>
2023-06-05 23:27:50 +0500

    feat: 8 kyu solutions

---
+++ 8 kyu/Is the string uppercase.ex
@@ -0,0 +1,44 @@
+# -----------------------------------------------------------
+# Is the string uppercase?
+# Task
+# Create a method to see whether the string is ALL CAPS.
+# 
+# Examples (input -> output)
+# "c" -> False
+# "C" -> True
+# "hello I AM DONALD" -> False
+# "HELLO I AM DONALD" -> True
+# "ACSKLDFJSgSKLDFJSKLDFJ" -> False
+# "ACSKLDFJSGSKLDFJSKLDFJ" -> True
+# 
+# In this Kata, a string is said to be in ALL CAPS whenever it does not contain any lowercase letter so any string containing no letters at all is trivially 
+# considered to be in ALL CAPS.
+# -----------------------------------------------------------
+
+defmodule StringUtils do
+  def upper_case?(str) do
+    upp = String.upcase(str)
+    str == upp
+  end
+end
+
+# -----------------------------------------------------------
+# License
+# Tasks are the property of Codewars (https://www.codewars.com/) 
+# and users of this resource.
+# 
+# All solution code in this repository 
+# is the personal property of Vladimir Rukavishnikov
+# (vladimirrukavishnikovmail@gmail.com).
+# 
+# Copyright (C) 2023 Vladimir Rukavishnikov
+# 
+# This file is part of the HungryVovka/Codewars-Elixir
+# (https://github.com/HungryVovka/Codewars-Elixir)
+# 
+# License is GNU General Public License v3.0
+# (https://github.com/HungryVovka/Codewars-Elixir/blob/main/LICENSE.md)
+# 
+# You should have received a copy of the GNU General Public License v3.0
+# along with this code. If not, see http://www.gnu.org/licenses/
+# -----------------------------------------------------------
